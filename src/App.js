9e8f6108ae6a129a966a802ec6c7689a22557521
MRB02 <boymirzayev2202@gmail.com>
2022-05-21 00:03:51 +0500

    incr and decr functions, useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState } from 'react';
 
 function App() {
+ 
+  const [count, setCount]=useState(0);
+
+  function incr(){
+    setCount(count+1)
+  }
+  function decr(){
+    setCount(count-1)
+  }
     return ( <>
-    
+    <h3>Count:{count}</h3>
+    <button 
+    onClick={incr}
+    className="btn btn-success">Incr</button>
+    <button 
+    onClick={decr}
+    className="btn btn-danger">Decr</button>
     </>
     )
 }
