04e90ae986aee36e5a831b84f05b78041d30129f
MRB02 <boymirzayev2202@gmail.com>
2022-05-20 23:44:38 +0500

    Reactni detalni o'rganish amaliyot+dars

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+    return ( <>
+    <h1>Qaley hayot</h1>
+    </>
+    )
+}
+
+export default App;
